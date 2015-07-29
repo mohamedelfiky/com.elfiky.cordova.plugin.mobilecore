9cf4663bec2e6600026e9c56c99d9b141ac912e7
mohamedelfiky <mohamed.elfikyy@yahoo.com>
2015-07-29 14:51:44 +0200

    show stickee ad

--- android/MobileCorePlugin.java
+++ android/MobileCorePlugin.java
@@ -13,50 +13,102 @@ import com.ironsource.mobilcore.UserProperties;
 import com.ironsource.mobilcore.UserProperties.AgeRange;
 import com.ironsource.mobilcore.UserProperties.Gender;
 
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 
 public class MobileCorePlugin extends CordovaPlugin {
 
-	public static final String ACTION_SHOW_INTERSTITIAL_ADD = "show_interstitial";
+	public static final String ACTION_SHOW_INTERSTITIAL_AD = "show_interstitial";
+	public static final String ACTION_SHOW_STICKEE_AD = "show_interstitial";
 
-	private final String DEV_HASH = "161LSETH6VFWNHL9PL5YCME1GM17C";
-	private final String SAMPLE_URL = "www.mobilecore.com";
+	private final String TAG = "mobilecore_log";
+
+	private static final String DEFAULT_DEV_HASH = "82VXL1FJZV6C1AKK2KSZQMW7NFPS4";
+
+	/* options */
+	private static final String OPT_DEV_HASH = "dev_hash";
+
+	private String dev_hash = DEFAULT_DEV_HASH;
 
 	@Override
-	public boolean execute(String action, JSONArray data,
+	public boolean execute(final String action, JSONArray data,
 			CallbackContext callbackContext) throws JSONException {
 
-		if (ACTION_SHOW_INTERSTITIAL_ADD.equals(action)) {
-			Log.v("MobileCore test", "test");
-			/*************************/
-
-			UserProperties user = new UserProperties().setGender(Gender.FEMALE)
-					.setAgeRange(AgeRange._18_24);
-			MobileCore.init(cordova.getActivity(), DEV_HASH, user,
-					LOG_TYPE.DEBUG, AD_UNITS.STICKEEZ, AD_UNITS.INTERSTITIAL,
-					AD_UNITS.DIRECT_TO_MARKET, AD_UNITS.NATIVE_ADS);
-
-			// If you will be using banners in your Ads listview call the
-			// following method after init
-			MobileCore.setNativeAdsBannerSupport(true);
-
-			// This button will call showInterstitial with forceShow = true (on
-			// screen response to no connection situations)
-			// Notice how it's possible to set a null value instead of a
-			// CallbackResponse if you don't want to react to user.
-			showInterstitial();
-
-			/*************************/
-			callbackContext.success("sucess");
-			return true;
-		} else {
-			return false;
+		if (ACTION_SHOW_INTERSTITIAL_AD.equals(action)) {
+			JSONObject options = data.optJSONObject(0);
+			executeCreateInterstitialView(options, callbackContext);
+		}else if(ACTION_SHOW_STICKEE_AD.equals(action)){
+			JSONObject options = data.optJSONObject(0);
+			executeCreateStickeeView(options, callbackContext);
 		}
 
+		return true;
+
 	}
 
-	private void showInterstitial() {
-		MobileCore.showInterstitial(cordova.getActivity(),
-				AD_UNIT_SHOW_TRIGGER.BUTTON_CLICK, null);
+	private PluginResult executeCreateInterstitialView(JSONObject options,
+			final CallbackContext callbackContext) {
+		this.setOptions(options);
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					MobileCore.init(cordova.getActivity(), dev_hash, null,
+							LOG_TYPE.DEBUG, AD_UNITS.STICKEEZ,
+							AD_UNITS.INTERSTITIAL, AD_UNITS.DIRECT_TO_MARKET,
+							AD_UNITS.NATIVE_ADS);
+					MobileCore.showInterstitial(cordova.getActivity(), null);
+
+					Log.v(TAG, "Show mobilecore ad Interstitial");
+				} catch (Exception ex) {
+					Log.e(TAG, "error error error error ");
+					Log.e(TAG, ex.getMessage());
+				}
+
+				callbackContext.success();
+			}
+		});
+
+		return null;
 	}
+
+	
+	private PluginResult executeCreateStickeeView(JSONObject options,
+			final CallbackContext callbackContext) {
+		this.setOptions(options);
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					MobileCore.init(cordova.getActivity(), dev_hash, null,
+							LOG_TYPE.DEBUG, AD_UNITS.STICKEEZ,
+							AD_UNITS.INTERSTITIAL, AD_UNITS.DIRECT_TO_MARKET,
+							AD_UNITS.NATIVE_ADS);
+					MobileCore.showStickee(cordova.getActivity());
+
+					Log.v(TAG, "Show mobilecore Stickee ad ");
+				} catch (Exception ex) {
+					Log.e(TAG, "error error error error ");
+					Log.e(TAG, ex.getMessage());
+				}
+
+				callbackContext.success();
+			}
+		});
+
+		return null;
+	}
+
+	
+	private void setOptions(JSONObject options) {
+		if (options == null)
+			return;
+
+		if (options.has(OPT_DEV_HASH))
+			this.dev_hash = options.optString(OPT_DEV_HASH);
+	}
+
 }
--- js/mobile_core.js
+++ js/mobile_core.js
@@ -2,16 +2,38 @@
  * Created by elfiky on 28/07/15.
  */
 
-var mobileCore = {
-  showInterstitial: function (successCallback, errorCallback) {
-    cordova.exec(
-      successCallback, // success callback function
-      errorCallback, // error callback function
-      'MobileCorePlugin', // mapped to our native Java class called "MobileCorePlugin"
-      'show_interstitial', // with this action name
-      []
-    );
-  }
+var mobileCore = function(dev_hash){
+  var _module = {
+
+
+    showInterstitial: function (successCallback, errorCallback) {
+      cordova.exec(
+        successCallback, // success callback function
+        errorCallback, // error callback function
+        'MobileCorePlugin', // mapped to our native Java class called "MobileCorePlugin"
+        'show_interstitial', // with this action name
+        [{
+          dev_hash: dev_hash
+        }]
+      );
+    },
+
+    showStickee: function (successCallback, errorCallback) {
+      cordova.exec(
+        successCallback, // success callback function
+        errorCallback, // error callback function
+        'MobileCorePlugin', // mapped to our native Java class called "MobileCorePlugin"
+        'show_stickee', // with this action name
+        [{
+          dev_hash: dev_hash
+        }]
+      );
+    }
+
+  };
+
+
+  return _module;
 };
 
 module.exports = mobileCore;
